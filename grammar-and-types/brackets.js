63333ffb91f8efede3b0db89bbb7b4a711ebb1b1
Arpi Parsyan <parsyanarpi@gmail.com>
2016-11-19 17:47:59 +0400

    add brackets problem
    Change let to const, add semicolons

---
+++ grammar-and-types/brackets.js
@@ -0,0 +1,20 @@
+/**
+* Topic: Grammar
+* Problem instructions: how can we solve the problem1?
+* Relevant resources: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Gram$
+*/
+function problem1(){
+  console.log c;
+  const r = 35;
+  const p = 19;
+  const c = r+p;
+}
+
+/**
+ * Solution - below is a base64 blob of the solution.
+ *
+--begin--
+ZnVuY3Rpb24gcHJvYmxlbTEoKXsNCiAgY29uc29sZS5sb2cgKGMpDQogIGxldCByPTM1DQogIGxldCBwPTE5DQog$
+--end--
+ *
+ */
